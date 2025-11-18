7773992c136100f83681bc494da4318970dc9d49
zingrin <jerinloncheu@gmail.com>
2025-11-18 16:11:16 +0600

    problems sloved 3

--- solution.ts
+++ solution.ts
@@ -15,3 +15,18 @@ const getLength = (value: string | any[]): number => {
   }
   return value.length;
 };
+
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
+}
