af5e16363e2f930aea8a0aa5c4b7753e6de55c54
zingrin <jerinloncheu@gmail.com>
2025-11-18 16:02:25 +0600

    Problems 1 sloving

---
+++ solution.ts
@@ -0,0 +1,9 @@
+const formatValue = (value: string | number | boolean): string | number | boolean => {
+  if (typeof value === 'string') {
+    return value.toUpperCase();
+  }
+  if (typeof value === 'number') {
+    return value * 10;
+  }
+  return !value;
+};
